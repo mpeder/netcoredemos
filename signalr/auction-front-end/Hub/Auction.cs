148be714a121d612c848ee191bb5a0d4750b9b65
Michael Pedersen <mpeder@microsoft.com>
2018-10-23 14:27:52 +0200

    added signalr

---
+++ signalr/auction-front-end/Hub/Auction.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace Microsoft.Azure.SignalR.Samples.AuctionFE
+{
+    public class Auction : Hub
+    {
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public void BroadcastMessageGroup(string name, string message, string auctionId)
+        {
+           Clients.Group(auctionId).SendAsync("broadcastMessage", name, message);
+           
+        }
+
+        public void AuctionEvent(string name, string message)
+        {
+            Clients.All.SendAsync("auctionevent", name, message);
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+
+        public void JoinAuction(string auctionId)
+        {
+           Groups.AddToGroupAsync(Context.ConnectionId, auctionId);
+        }
+
+        public void LeaveAuction(string auctionId)
+        {
+            Groups.RemoveFromGroupAsync(Context.ConnectionId, auctionId);
+        }
+        
+    }
+}
