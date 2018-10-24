0ce957ee137d5d014b16abf03a4aab355d3cf024
Michael Pedersen <mpeder@microsoft.com>
2018-10-24 11:18:15 +0200

    analysis sample added

---
+++ migrate/fullframework/RegistryReaderTool/RegistryReaderTool/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistryReaderTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\BIOS");
+            if (key != null)
+            {
+                Console.WriteLine("BIOS Values:");
+                foreach (var valuename in key.GetValueNames())
+                {
+                    Console.WriteLine(valuename + ": " + key.GetValue(valuename).ToString());
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
